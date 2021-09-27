c8650c56bae13300a617091c874b5c78040bb442
mynt1vmo <mynt1@vmodev.com>
2021-09-27 16:34:18 +0700

    feature/button-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Button from "components/button";
+
 function App() {
+  const sumb = () => {
+    console.log("hihi");
+  };
   return (
     <div className="App">
       <header className="App-header">Booke Admin</header>
+      <Button onClick={sumb}>add</Button>
     </div>
   );
 }
