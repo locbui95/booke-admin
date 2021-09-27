0d506266bc2bf886309d9a7b50e2eda7e723cf1a
PNChien <hoddary.2308@gmail.com>
2021-09-27 13:20:30 +0700

    feature: header-component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,10 @@
 import Search from "components/search";
 import avatar from "../../assets/avt.jpg";
+import "../../layout/app/styles.css";
 
 export default function Header() {
   return (
-    <div className="fixed top-0 right-0 left-0 flex justify-between my-10 pl-96">
+    <div className="fixed top-0 right-0 flex justify-between py-10 px-10 bg-gray-100 content">
       <Search />
       <div className="relative flex items-center mr-10">
         <div className="realtive w-10 h-10 rounded-full overflow-hidden">
