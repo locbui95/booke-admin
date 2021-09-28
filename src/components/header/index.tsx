c20af3c1c63c48c81cb621c8505826f18df944dd
PNChien <hoddary.2308@gmail.com>
2021-09-28 17:28:52 +0700

    feature: header-component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,10 @@
-import Search from "components/search";
 import avatar from "assets/avt.jpg";
 import "./header.styles.css";
 
 export default function Header() {
-  const handleSearch = (search: string) => {
-    alert(search);
-  };
-
   return (
-    <div className="fixed top-0 right-0 flex justify-between py-10 px-10 bg-gray-100 content">
-      <Search onSearch={handleSearch} />
-      <div className="relative flex items-center mr-10">
+    <div className="fixed top-0 right-0 flex justify-end h-24 items-center content shadow-sm">
+      <div className="flex mr-10">
         <div className="realtive w-10 h-10 rounded-full overflow-hidden">
           <img alt="avatar" src={avatar} />
         </div>
