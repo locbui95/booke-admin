8a54bae8ffdbc84ac0f5a9b4d8b3a87563cd41dd
PNChien <hoddary.2308@gmail.com>
2021-09-28 12:50:28 +0700

    refactor:sidebar-component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,4 @@
-import { AiFillAppstore, AiOutlineMenuUnfold } from "react-icons/ai";
-import { BsFillInboxesFill } from "react-icons/bs";
+import { AiFillAppstore, AiOutlineMenuUnfold, AiFillHdd } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 import logo from "assets/logo.png";
@@ -9,9 +8,9 @@ export default function Sidebar() {
   return (
     <div className="fixed top-0 left-0 w-80 p-5 bottom-0 bg-white shadow-xl">
       <div className="my-10 text-4xl text-center">
-        <a className="flex justify-center">
+        <Link to="/" className="flex justify-center">
           <img className="w-40" src={logo} alt="" />
-        </a>
+        </Link>
       </div>
       <nav className="pt-7 text-center">
         <Link
@@ -31,7 +30,7 @@ export default function Sidebar() {
           to={PATH_PRODUCTS}
           className="flex py-4 cursor-pointer text-md text-left mx-2 rounded-2xl hover:bg-blue-400 hover:text-white transition-all"
         >
-          <BsFillInboxesFill className="text-2xl mx-3" />
+          <AiFillHdd className="text-2xl mx-3" />
           Management Product
         </Link>
       </nav>
