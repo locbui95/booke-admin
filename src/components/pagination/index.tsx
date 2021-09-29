cd198311f13820f3bd7ba1ecebdf324780cba86b
IceI3ear <vietviet94230@gmail.com>
2021-09-29 17:59:14 +0700

    feature:pagination-componet

---
+++ src/components/pagination/index.tsx
@@ -0,0 +1,67 @@
+import Button from "components/button";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import clsx from "clsx";
+
+import { usePagination, DOTS } from "./usePagination";
+
+const Pagination = (props: any) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize
+  } = props;
+
+  const paginationRange: any = usePagination(
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize
+  );
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  return (
+    <ul className="flex items-center justify-end m-2">
+      <Button
+        className="text-black py-0 px-3 h-8 text-center box-border flex items-center rounded-2xl hover:bg-blue-400 transition-all hover:text-white cursor-pointer"
+        onClick={onPrevious}
+        disabled={currentPage === 1}
+      >
+        <FaAngleLeft />
+      </Button>
+
+      {paginationRange?.map((pageNumber: number | string) => (
+        <Button
+          className={clsx(
+            "text-black py-0 px-3 h-8 text-center box-border flex items-center rounded-2xl",
+            pageNumber !== DOTS &&
+              "hover:bg-blue-400 transition-all hover:text-white cursor-pointer"
+          )}
+          key={Math.random()}
+          onClick={() => onPageChange(pageNumber)}
+          disabled={pageNumber === DOTS}
+        >
+          {pageNumber}
+        </Button>
+      ))}
+
+      <Button
+        className="text-black py-0 px-3 h-8 text-center box-border flex items-center rounded-2xl hover:bg-blue-400 transition-all hover:text-white cursor-pointer"
+        onClick={onNext}
+        disabled={currentPage === Math.ceil(totalCount / pageSize)}
+      >
+        <FaAngleRight />
+      </Button>
+    </ul>
+  );
+};
+
+export default Pagination;
