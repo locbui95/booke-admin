02779fca9756d418e1d232def4f80ae6b0ed5ce2
linhnhVMO <88028551+linhnhVMO@users.noreply.github.com>
2021-09-29 21:22:46 +0700

    feature: select component

---
+++ src/components/select/index.tsx
@@ -0,0 +1,13 @@
+import React, { ComponentPropsWithoutRef } from "react";
+import styles from "./select.module.css";
+
+interface SelectProps extends ComponentPropsWithoutRef<"select"> {
+  children: React.ReactNode;
+}
+
+const Select = ({ children, ...props }: SelectProps) => (
+  <div className={styles.select}>
+    <select {...props}>{children}</select>
+  </div>
+);
+export default Select;
