73cd0d527802caa891f7b56341ad0dac6e687cf5
PNChien <hoddary.2308@gmail.com>
2021-09-30 11:21:35 +0700

    feature: products-component

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+import Button from "components/button";
+import Search from "components/search";
+import Select from "components/select";
 
 export default function Products() {
-  return <div>Products</div>;
+  const handleSearch = (search: string) => {
+    console.log(search);
+  };
+
+  return (
+    <div>
+      <div className="flex items-center bg-white p-8 shadow-sm justify-between">
+        <p className="font-bold text-xl">Product</p>
+        <div className="w-3/4 flex justify-end">
+          <div className="w-1/4 mr-10">
+            <Select>
+              <option disabled className="leading-10" value="number">
+                number
+              </option>
+              <option className="leading-10" value="1">
+                one
+              </option>
+              <option className="leading-10" value="2">
+                two
+              </option>
+              <option className="leading-10" value="2">
+                three
+              </option>
+            </Select>
+          </div>
+          <Search onSearch={handleSearch} />
+          <Button
+            loading={false}
+            className="flex p-3 bg-blue-400 hover:bg-blue-500 text-white"
+          >
+            <AiOutlinePlus className="pr-2 text-2xl font-bold" />
+            Add Product
+          </Button>
+        </div>
+      </div>
+      <div className="mt-10">
+        <p>List product</p>
+      </div>
+    </div>
+  );
 }
