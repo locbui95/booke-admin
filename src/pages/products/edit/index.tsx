a514f4a3949b4aa43a7b8784aa9488451bdee55f
PNChien <hoddary.2308@gmail.com>
2021-09-30 23:50:04 +0700

    feature: product-component

---
+++ src/pages/products/edit/index.tsx
@@ -0,0 +1,3 @@
+export default function EditProduct() {
+  return <div>edit</div>;
+}
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,48 +1,9 @@
-import { AiOutlinePlus } from "react-icons/ai";
-
-import Button from "components/button";
-import Search from "components/search";
-import Select from "components/select";
+import SearchBar from "modules/products/search-bar";
 
 export default function Products() {
-  const handleSearch = (search: string) => {
-    console.log(search);
-  };
-
   return (
     <div>
-      <div className="flex items-center bg-white p-8 shadow-sm justify-between">
-        <p className="font-bold text-xl">Product</p>
-        <div className="w-3/4 flex justify-end">
-          <div className="w-1/4 mr-10">
-            <Select>
-              <option disabled className="leading-10" value="number">
-                number
-              </option>
-              <option className="leading-10" value="1">
-                one
-              </option>
-              <option className="leading-10" value="2">
-                two
-              </option>
-              <option className="leading-10" value="2">
-                three
-              </option>
-            </Select>
-          </div>
-          <Search onSearch={handleSearch} />
-          <Button
-            loading={false}
-            className="flex p-3 bg-blue-400 hover:bg-blue-500 text-white"
-          >
-            <AiOutlinePlus className="pr-2 text-2xl font-bold" />
-            Add Product
-          </Button>
-        </div>
-      </div>
-      <div className="mt-10">
-        <p>List product</p>
-      </div>
+      <SearchBar />
     </div>
   );
 }
