36d118dd5fa61764c8ed72edaa15c6a59b98b8c6
linhnhVMO <88028551+linhnhVMO@users.noreply.github.com>
2021-09-30 09:54:43 +0700

    chore: select component

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,9 +1,11 @@
 import React, { ComponentPropsWithoutRef } from "react";
+
 import styles from "./select.module.css";
 
 interface SelectProps extends ComponentPropsWithoutRef<"select"> {
   children: React.ReactNode;
 }
+
 const Select = ({ children, ...props }: SelectProps) => (
   <div className={styles.select}>
     <select {...props}>{children}</select>
