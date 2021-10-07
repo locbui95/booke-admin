ad31b2c6de714a77d0b601e52d8187fb22d7e0d1
PNChien <hoddary.2308@gmail.com>
2021-10-07 16:22:09 +0700

    refactor: setup store user

---
+++ src/store/users/constant.ts
@@ -0,0 +1,18 @@
+import User from "types/user";
+
+export const PENDING = "PENDING";
+export const GET_USERS = "GET_USERS";
+export const DELETE_USER = "DELETE_USER";
+export const ADD_USER = "ADD_USER";
+export const GET_USER_DETAIL = "GET_USER_DETAIL";
+export const UPDATE_USER = "UPDATE_USER";
+export const REJECTED = "REJECTED";
+
+export type ActionTypes =
+  | { type: typeof PENDING }
+  | { type: typeof GET_USERS; payload: User[] }
+  | { type: typeof DELETE_USER; payload: number }
+  | { type: typeof ADD_USER; payload: User }
+  | { type: typeof GET_USER_DETAIL; payload: User }
+  | { type: typeof UPDATE_USER; payload: User }
+  | { type: typeof REJECTED };
