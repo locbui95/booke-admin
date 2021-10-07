2472f47adc8c56fcdbb406f0cff9c5cb7ae04991
IceI3ear <vietviet94230@gmail.com>
2021-10-07 13:59:41 +0700

    resolve-conflict

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
 export { default as Dashboard } from "./dashboard";
 export { default as Categories } from "./categories";
 export { default as Product } from "./product/list";
-export { default as Login } from "./login";
