9956f9b0b1df9fb76d9e94aa1c989aaf8b7879fc
IceI3ear <vietviet94230@gmail.com>
2021-10-12 17:24:41 +0700

    freature/login-logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import Layout from "layout";
 import Login from "pages/login";
 
 function App() {
+  const { infoUser: user } = useSelector((state: RootState) => state.users);
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact={true} path="/login" component={Login} />
-          <Route exact={false} path="/" component={Layout} />
+          <Route
+            exact={false}
+            path="/"
+            render={() =>
+              Object(user).email ? <Layout /> : <Redirect to="/login" />
+            }
+          />
         </Switch>
       </Router>
     </div>
